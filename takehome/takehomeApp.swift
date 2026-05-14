395caffc76b8161c324360e69994c1d2b88e78d1
Robert Avellar <robertavellar@gmail.com>
2026-05-13 20:06:28 -0700

    initial commit

---
+++ takehome/takehomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  takehomeApp.swift
+//  takehome
+//
+//  Created by Robert Avellar on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct takehomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
